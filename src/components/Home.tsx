e2c84964e805a5aee1ecc4cff30746906001024a
kinako1415 <Owata09@outlook.jp>
2025-02-27 00:28:34 +0900

    投稿のデザイン作成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,22 @@
+import "./Home.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はreactの学習中です。これから頑張ってreactエンジニアとして活躍していきたいです。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>tomato</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
